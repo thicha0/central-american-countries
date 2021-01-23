1c922aaae2590024834ce36f06b879046c03b365
Thicha <tib.chatelain@hotmail.fr>
2021-01-23 17:44:40 +0100

    styling

--- src/components/Expand.vue
+++ src/components/Expand.vue
@@ -3,14 +3,14 @@
         <div
             v-for="(item, index) in items"
             :key="index"
-            @click="activeIndex = index"
+            @click="activeIndex === index ? activeIndex = null : activeIndex = index"
             class="panel"
             :style="panelStyle(item, index)"
         >
-            <div class="background" :style="backgroundStyle(item, index)"></div>
-            <div class="gradient" :style="gradientStyle(index)"></div>
-            <h3 class="title" :style="titleStyle(index)">{{ item.title }}</h3>
-            <p class="text" :style="textStyle(index)">{{ item.text }}</p>
+            <div class="background" :style="{ backgroundImage: item.background }"></div>
+            <div class="gradient"></div>
+            <h3 class="title">{{ item.title }}</h3>
+            <p class="text">{{ item.text }}</p>
         </div>
     </div>
 </template>
@@ -31,63 +31,21 @@
             },
 
             // Container
-            width: {
+            width: { // TODO leave ?
                 type: String,
                 default: '90vw'
             },
 
             // Panel
-            height: {
+            height: { // TODO leave ?
                 type: String,
                 default: '80vh'
             },
-            borderRadius: {
-                type: String,
-                default: '20px'
-            },
-            margin: {
-                type: String,
-                default: '10px'
-            },
 
             // Panel flex
-            flexActive: {
-                type: Number,
-                default: 5
-            },
-            flexInactive: {
-                type: Number,
-                default: 0.8
-            },
             flexTransitionDuration: {
                 type: String,
                 default: '1.4s'
-            },
-
-            // Background
-            grayscaleInactive: {
-                type: String,
-                default: '65%'
-            },
-
-            // Title
-            titleColor: {
-                type: String,
-                default: 'white'
-            },
-            titleSize: {
-                type: String,
-                default: '24px'
-            },
-
-            // Text
-            textColor: {
-                type: String,
-                default: 'white'
-            },
-            textSize: {
-                type: String,
-                default: '20px'
             },
         },
         methods: {
@@ -96,74 +54,9 @@
                     'width':      this.width,
                 }
             },
-            panelStyle (item, index) {
+            panelStyle () {
                 return {
                     'height':           this.height,
-                    'border-radius':    this.borderRadius,
-                    'margin':           this.margin,
-
-                    // Flex
-                    'flex': index === this.activeIndex ?
-                        this.flexActive + ' 1 auto' :
-                        this.flexInactive + ' 1 auto',
-                    'transition': 'flex ' + this.flexTransitionDuration + ' ease',
-                }
-            },
-            backgroundStyle (item, index) {
-                return {
-                    // Props
-                    'background-image':     item.background,
-                    'filter': index === this.activeIndex ?
-                        'grayscale(0)' :
-                        'grayscale(' + this.grayscaleInactive + ')',
-
-                    // Transition(s)
-                    'transition':   'filter 1s ease',
-                }
-            },
-            gradientStyle (index) {
-                return {
-                    // Background
-                    'background':
-                        index === this.activeIndex ?
-                            'linear-gradient(to bottom, rgba(0,0,0,0) 20%,rgba(0,0,0,1) 95%)'
-                            : '',
-
-                    // Transition
-                    'opacity':      index === this.activeIndex ? 1 : 0,
-                    'transform':    index === this.activeIndex ?
-                        'translateY(0)' :
-                        'translateY(100px)',
-                    'transition':   'all 0.3s ease-in ' + this.flexTransitionDuration,
-                }
-            },
-            titleStyle (index) {
-                return {
-                    // Props
-                    'color':        this.titleColor,
-                    'font-size':    this.titleSize,
-
-                    // Transition
-                    'opacity':      index === this.activeIndex ? 1 : 0,
-                    'transition':   'opacity 0.3s ease-in 0.4s',
-                }
-            },
-            textStyle (index) {
-                return {
-                    // Props TODO
-                    'color':        this.textColor,
-                    'font-size':    this.textSize,
-
-                    // Transition
-                    'opacity':      index === this.activeIndex ? 1 : 0,
-                    'transition':   index === this.activeIndex ?
-                        // Slow in
-                        'all 0.3s ease-in ' + this.flexTransitionDuration :
-                        // Fast out
-                        'opacity 0.1s ease-out 0s',
-                    'transform':    index === this.activeIndex ?
-                        'translateY(0)' :
-                        'translateY(25px)',
                 }
             },
         }
